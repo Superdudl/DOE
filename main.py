520a6396851d7d7d6d16d8141196fadf8cd85339
Superdudl <klyuev.vladisalv01@mail.ru>
2025-04-23 22:37:42 +0300

    Режим отладки

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ import sys
 sys.path.append(__file__)
 
 from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QSettings
 from view.MainWindow import MainWindow
-from controllers import MainController
+from pathlib import Path, PurePath
+
+DEBUG = True
 
 def main():
+    path = Path(PurePath(__file__).parent) / 'settings' / 'settings.ini'
+    settings = QSettings(str(path), QSettings.Format.IniFormat)
+    settings.setValue('config/DEBUG', DEBUG)
+
     app = QApplication(sys.argv)
+    from controllers import MainController
 
     window = MainWindow()
     window.showMaximized()
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,16 @@
 import sys
 sys.path.append(__file__)
 
-from .config import DEBUG
-
 from .videoCapture import VideoCapture
 from .inference import Inference
 from .videoStream import VideoStream
+
+from PySide6.QtCore import QSettings
+from pathlib import Path, PurePath
+
+class CONFIG:
+    _path = Path(PurePath(__file__).parents[1]) / 'settings' / 'settings.ini'
+    _settings = QSettings(str(_path), QSettings.Format.IniFormat)
+    DEBUG = _settings.value('config/DEBUG', type=bool)
+
+DEBUG = CONFIG.DEBUG
