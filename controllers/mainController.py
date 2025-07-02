dbf9429cb76cb635e112276b4680159d92a1aa93
Superdudl <klyuev.vladisalv01@mail.ru>
2025-07-02 11:21:04 +0300

    Добавлен раздел СПРАВКА

--- controllers/mainController.py
+++ controllers/mainController.py
@@ -2,12 +2,13 @@ import sys
 
 sys.path.append(__file__)
 
+from pathlib import Path, PurePath
 from PySide6.QtCore import QObject, Slot
 from PySide6.QtGui import QPixmap
 from PySide6.QtWidgets import QMessageBox, QFileDialog
 from view.MetricsDialog import MetricsDialog
 from controllers import CameraController, InferenceController, RecordController, VideoReader, AnalyseController
-from utils import VideoStream
+from utils import VideoStream, open_pdf
 from utils import DEBUG
 
 
@@ -28,10 +29,27 @@ class MainController(QObject):
         self.ui.connect_camera.triggered.connect(self.connect_camera)
         self.ui.load_video.triggered.connect(self.open_video)
         self.ui.analise.triggered.connect(self.open_analise_dialog)
+        self.ui.action.triggered.connect(self.open_reference)
 
     def __del__(self):
         self.video_stream.stop_stream()
         self.inference_controller.stop()
+
+    def open_reference(self):
+        path = Path(__file__).parents[1] / 'src'
+        sender = self.sender().text()
+        if sender == 'Спецификация':
+            path = path.joinpath('Спецификация.pdf')
+        elif sender == 'Текст программы':
+            path = path.joinpath('Текст программы.pdf')
+        elif sender == 'Описание программы':
+            path = path.joinpath('Описание программы.pdf')
+        elif sender == 'Руководство оператора':
+            path = path.joinpath('Руководство оператора.pdf')
+        elif sender == 'Руководство системного программиста':
+            path = path.joinpath('Руководство системного программиста.pdf')
+
+        open_pdf(path)
 
     @Slot()
     def connect_camera(self):
--- utils/__init__.py
+++ utils/__init__.py
@@ -5,7 +5,9 @@ from .videoCapture import VideoCapture
 from .inference import Inference
 from .videoStream import VideoStream
 
-from PySide6.QtCore import QSettings
+from PySide6.QtCore import QSettings, QUrl
+from PySide6.QtGui import QDesktopServices
+from PySide6.QtWidgets import QMessageBox
 from pathlib import Path, PurePath
 
 class CONFIG:
@@ -14,3 +16,11 @@ class CONFIG:
     DEBUG = _settings.value('config/DEBUG', type=bool)
 
 DEBUG = CONFIG.DEBUG
+
+def open_pdf(filepath):
+    path = Path(filepath)
+    if path.exists():
+        QDesktopServices.openUrl(QUrl.fromLocalFile(path))
+    else:
+        QMessageBox.warning(None, 'Ошибка', 'Файл не найден')
+
